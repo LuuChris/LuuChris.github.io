4615b7f8817c36f70728930d32a79d271e59671b
LuuChris <christopherdhluu@gmail.com>
2024-03-03 00:22:17 -0500

    Basic Maven Generated Repository

---
+++ src/main/java/com/homepage/cvpage/CvpageApplication.java
@@ -0,0 +1,13 @@
+package com.homepage.cvpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvpageApplication.class, args);
+	}
+
+}
